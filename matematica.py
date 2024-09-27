daffe83f35280a2de3a6aba6a76c8a61acaa969e
CocaQuente <blacluiz7@gmail.com>
2024-09-27 13:21:36 +0000

    Exemplo de comandos basicos em python

---
+++ matematica.py
@@ -0,0 +1,6 @@
+def area(largura, profundidade):
+    return largura * profundidade
+def volume(largura, profundidade, altura):
+    return area(largura,profundidade) * altura
+print(area(2,3))
+print(volume(2,3,5))
---
+++ ola.py
@@ -0,0 +1,12 @@
+# Criando o comando ola
+nome= 'Joao' #variavel global
+def ola(nome): # variavel local
+    print(f'Olá {nome}! Seja bem vindo(a)!')
+    print(f'Ola {nome}!')
+    ola('João')
+ola(input('Digite seu nome:'))
+
+    
+
+
+
